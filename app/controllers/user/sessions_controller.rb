d4a35294b0fefcb09f02a88fad95ceee3f259a81
khodir <khodir94@gmail.com>
2025-08-13 09:31:14 +0700

    feat: enhance user login process with remember me functionality and improved error handling

--- app/controllers/user/sessions_controller.rb
+++ app/controllers/user/sessions_controller.rb
@@ -13,15 +13,19 @@ class User::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   def create
-    p = params.permit(:email, :password)
-    creds = UserLoginRequest.new(email: p[:email], password: p[:password])
+    p = params.permit(:email, :password, :remember_me)
+    creds = UserLoginRequest.new(p.to_h)
     creds.validate!
 
     @user = User.find_by(email: creds.email)
-    return redirect_back fallback_location: root_path, inertia: { errors: { email: "Invalid Email or Password" } } if @user.nil?
+    return redirect_back fallback_location: root_path, inertia: { errors: { email: [ "Invalid Email or Password" ] } } if @user.nil?
 
     if @user&.valid_password?(creds.password)
+      @user.update_tracked_fields!(request)
+      @user.save!
+
       sign_in(@user)
+      remember_me(@user) if creds.remember_me
       redirect_to root_path, inertia: { flash: { notice: "Signed in successfully." } }
     else
       redirect_back fallback_location: root_path, inertia: { errors: format_validation_errors(@user&.errors || creds.errors) }
--- app/requests/user_login_request.rb
+++ app/requests/user_login_request.rb
@@ -1,6 +1,6 @@
 class UserLoginRequest
   include ActiveModel::Model
 
-  attr_accessor :email, :password
+  attr_accessor :email, :password, :remember_me
   validates_presence_of :email, :password
 end
