8fdc4c564e7af3dd0f932d9acc794a75cd83044b
khodir <khodir94@gmail.com>
2025-08-13 11:24:43 +0700

    feat: add error handling for ActiveModel validation errors in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
 
+  # error handling
+  rescue_from ActiveModel::ValidationError, with: :handle_validation_errors
+
   # filters
   before_action :authenticate_user!
 
