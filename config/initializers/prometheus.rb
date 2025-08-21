f1fcc8aa66d29b1a5cd57418561c89b091850e92
khodir <khodir94@gmail.com>
2025-08-21 11:10:38 +0700

    feat: enable Prometheus metrics collection in all Rails environments

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,6 +1,6 @@
 require "prometheus_exporter/middleware"
 
-if Rails.env.production? && ENV.fetch("ENABLE_PROMETHEUS", "false") == "true"
+if ENV.fetch("PROMETHEUS_ENABLED", "false") == "true"
   # This reports stats per request like HTTP status and timings
   Rails.application.middleware.unshift PrometheusExporter::Middleware, instrument: :prepend
 end
