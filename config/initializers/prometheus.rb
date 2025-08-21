22b223e20740d189c3026034494d87d9ab197759
khodir <khodir94@gmail.com>
2025-08-21 10:46:07 +0700

    feat: add environment flag to conditionally enable Prometheus monitoring

--- config/initializers/prometheus.rb
+++ config/initializers/prometheus.rb
@@ -1,6 +1,7 @@
 require "prometheus_exporter/middleware"
 
-if Rails.env.production?
+enable_prometheus = ENV.fetch("ENABLE_PROMETHEUS") { false }
+if Rails.env.production? && !!enable_prometheus
   # This reports stats per request like HTTP status and timings
   Rails.application.middleware.unshift PrometheusExporter::Middleware, instrument: :prepend
 end
